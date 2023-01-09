53558385decb179ff175cbd6a268a89c4e9ed4a0
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-01-08 20:20:25 -0600

    GET employees

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import express from "express";
 import employeesRoutes from "./routes/employees.routes.js";
-import IndexRoutes from './routes/index.routes.js'
+import IndexRoutes from "./routes/index.routes.js";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 // routes
-app.use(employeesRoutes);
-app.use(IndexRoutes)
+app.use("/api", employeesRoutes);
+app.use(IndexRoutes);
 
 app.listen(3000);
 console.log(`Server running on port ${3000}`);
