708e7505a07bd809ab5f2ff9d886b31517f5ae2a
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-01-09 19:15:39 -0600

    Not found

--- src/index.js
+++ src/index.js
@@ -10,5 +10,12 @@ app.use(express.json());
 app.use("/api", employeesRoutes);
 app.use(IndexRoutes);
 
+// 404 handler
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: "endpoint not found",
+  });
+});
+
 app.listen(3000);
 console.log(`Server running on port ${3000}`);
